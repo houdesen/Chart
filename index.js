962554774952bb985a2a8466bc2c5a8fc68f62ce
houdesen <zh06101019>
2020-03-20 22:32:11 +0800

    v0.2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-$(function(){
+$(function () {
+    var myChart = echarts.init(document.getElementById('main'));
+    var container = document.getElementById('example');
+
+
     var data = [
-        
+
         ['Java', 1, '降', '-0.01%'],
         ['C', 2, '升', '+2.44%'],
         ['Python', 3, '升', '+1.41%'],
@@ -8,28 +12,26 @@ $(function(){
         ['C#', 5, '升', '+2.07%'],
         ['Visual Basic .NET', 6, '降', '-1.17%'],
         ['JavaScript', 7, '降', '-0.85%'],
-      ];
-      
-      var container = document.getElementById('example');
-      var hot = new Handsontable(container, {
+    ];
+
+    Handsontable(container, {
         data: data,
-        colHeaders:['语言名称', '排名', '升或降', '变化幅度'],
+        colHeaders: ['语言名称', '排名', '升或降', '变化幅度'],
         autoColumnSize: true,
         rowHeights: 45,
         colWidths: 200,
         className: "htCenter htMiddle",
-        
-      });
 
-    
-    var myChart = echarts.init(document.getElementById('main'));
-    var option = {
+    });
+
+
+    myChart.setOption({
         title: {
             text: 'JavaScript语言排名变化'
         },
         tooltip: {},
         xAxis: {
-            data: ["2000","2005","2010","2015","2020"]
+            data: ["2000", "2005", "2010", "2015", "2020"]
         },
         yAxis: {
             type: 'value'
@@ -39,7 +41,5 @@ $(function(){
             type: 'line',
             data: [6, 9, 8, 8, 7]
         }]
-    };
-
-    myChart.setOption(option);
+    });
 })
